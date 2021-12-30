427ba08972bd732f37f9ab1393bb226072bcc28c
Anik-Nath <anikdevnath03112000@gmail.com>
2021-12-30 16:32:51 +0600

    movie details page completed

--- src/Component/MovieDetails/MovieDetails.js
+++ src/Component/MovieDetails/MovieDetails.js
@@ -19,19 +19,19 @@ const MovieDetails = () => {
   return (
     <div style={{ background: "rgba(0, 1, 10, 0.96)" }} className="py-5 mt-5">
       <div className="container">
-        <div className="row text-light">
-          <div className="col-4">
+        <div className="row text-light px-3 px-lg-0 px-md-0">
+          <div className="col-lg-4 col-12 col-md-4">
             <img
-              className="w-100"
+              className="w-100 mb-3"
               src={`http://image.tmdb.org/t/p/w500${details.poster_path}`}
               alt=""
             />
           </div>
-          <div className="col-8">
+          <div className="col-lg-8 col-12 col-md-8">
             <h2 className="text-light">{details.original_title}</h2>
             <p>Release Date : {details.release_date}</p>
-            <p>Language : {details.original_language}</p>
-            <div className="d-flex align-items-center justify-content-between  gap-4 border border-start-0 border-end-0 py-3">
+            <p className="mb-5">Language : {details.original_language}</p>
+            <div className="d-flex flex-lg-row flex-column  justify-content-between  gap-lg-4 border border-start-0 border-end-0 py-3">
               <div id="rating">
                 <h6 className="d-flex align-items-center gap-2">
                   <i className="fas fa-star me-2 text-warning fs-6"></i>
@@ -53,7 +53,7 @@ const MovieDetails = () => {
                 />
               </div>
             </div>
-           
+
             <div id="overview" className="overview my-4">
               <h5 className="text-warning">Overview</h5>
               <p style={{ textAlign: "justify" }}>{details.overview}</p>
